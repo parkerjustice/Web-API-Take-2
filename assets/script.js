f7e91d66367c227f688f511bcb85e39cc8681173
parkerjustice <parkerjpiano@gmail.com>
2021-11-07 22:51:27 -0500

    added the var

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var timeLeft = 180;
+var clockId;
+var clockEl = document.getElementById("#clock");
+var startBtn = document.getElementById("startbtn");
+var nextBtn = document.getElementById("#next-btn");
+var questionSection = document.getElementById("question");
+var startSection = document.getElementById("#start-section");
+var questionsEl = document.getElementById("#question");
+var answerBtnEl = document.getElementById("#ans-btn");
+var ansCheckEl = document.getElementById("#ans-check");
+var highScore = document.getElementById("#highscores")
+var submitButton =document.getElementById("#submitbtn")
+var clearBtn = document.getElementById("#clear")
+var initialEl = document.getElementById("#initials-spot")
+var restartBtn = document.getElementById("#restartbtn")
